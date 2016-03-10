d3264d91c1a80243ca874245bd9ec580ede7716f
Robert Stiff <uatecuk@gmail.com>
2016-03-10 23:17:31 +0000

    Text is redundant really.

--- src/components/svg/index.js
+++ src/components/svg/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     Image: require('./Image.jsx'),
     Circle: require('./Circle.jsx'),
-    Line: require('./Line.jsx'),
-    Text: require('./Text.jsx')
+    Line: require('./Line.jsx')
 };
